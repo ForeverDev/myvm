db83147ab11f65f7497b55bd0c33317a568ac311
David Wilson <david.wilson@govsacademy.org>
2016-11-16 20:09:05 -0400

    update

--- lex.c
+++ lex.c
@@ -18,13 +18,13 @@ print_token_list(TokenList* list) {
 	for (TokenList* i = list; i; i = i->next) {
 		switch (i->value->type) {
 			case TOK_NUMBER:
-				printf("(TYPE: NUMBER, VALUE: %d)\n", i->value->nval);
+				printf("(TYPE: NUMBER,     VALUE: %d)\n", i->value->nval);
 				break;
 			case TOK_IDENTIFIER:
 				printf("(TYPE: IDENTIFIER, VALUE: '%s')\n", i->value->sval);
 				break;
 			case TOK_OPERATOR:
-				printf("(TYPE: OPERATOR, VALUE: %c)\n", i->value->oval);
+				printf("(TYPE: OPERATOR,   VALUE: '%c')\n", i->value->oval);
 				break;
 		}
 	}
@@ -80,38 +80,70 @@ static void
 lex_generate_tokens(LexState* lexer) {
 	/* this is where tokens are generated... */
 	char* scan = lexer->contents;
+	unsigned int line = 0;
 	char* buf; /* temporary buffer that reads a string */
 	char* at;  /* reader */
 	size_t length; /* length of buf */
 	Token* append;
 	while (*scan) {
 		at = scan;
-		if (*at == ' ') {
+		if (isspace(*at)) {
 			scan++;
 			continue;
-		} else if (ispunct(*at)) {
+		} else if (*at == '\n') {
+			line++;
+			scan++;
+			continue;
+		}
+
+		if (ispunct(*at)) {
 			append = malloc(sizeof(Token));
-			append->line = 0;
+			append->line = line;
 			append->type = TOK_OPERATOR;
 			append->oval = *at;
 			append_token(lexer, append);
 			scan++;
 		} else if (isdigit(*at)) {
+			int base = (*at == '0' && at[1] == 'x') ? 16 : 10;
+			/* if hex, skip over '0x' */
+			if (base == 16) {
+				at += 2;
+				scan += 2; /* scan also must skip '0x' */
+				if (!isdigit(*at) && !(*at >= 'A' && *at <= 'F')) {
+					printf("expected number to follow '0x'\n");
+					exit(1);
+				}
+			}
 			length = 0;
-			while (isdigit(*at)) {
+			while (isdigit(*at) || (base == 16 && *at >= 'A' && *at <= 'F')) {
 				length++;
 				at++;
 			}
 			buf = malloc(length + 1);
 			strncpy(buf, scan, length);
+			buf[length] = 0;
 			scan += length;
 			append = malloc(sizeof(Token));
-			append->line = 0;
+			append->line = line;
 			append->type = TOK_NUMBER;
-			append->nval = (uint8_t)strtol(buf, NULL, 10);
+			append->nval = (uint8_t)strtol(buf, NULL, base);
 			append_token(lexer, append);
 			free(buf);
 			buf = NULL;
+		} else if (isalpha(*at) || *at == '_') {
+			length = 0;
+			while (isalnum(*at) || *at == '_') {
+				length++;
+				at++;
+			}
+			append = malloc(sizeof(Token));
+			append->line = line;
+			append->type = TOK_IDENTIFIER;
+			append->sval = malloc(length + 1);
+			strncpy(append->sval, scan, length);
+			append->sval[length] = 0;
+			append_token(lexer, append);
+			scan += length;
 		} else {
 			scan++;
 		}
